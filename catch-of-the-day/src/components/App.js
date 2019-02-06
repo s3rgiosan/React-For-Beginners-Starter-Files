69859d69b6feb252f9dabeee96d1532615bc25e0
Sérgio Santos <me@s3rgiosan.com>
2019-02-06 17:58:39 +0000

    Lecture 1-13

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+
+class App extends Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    const fishes = { ...this.state.fishes };
+
+    fishes[`fish${Date.now()}`] = fish;
+
+    this.setState({
+      fishes: fishes
+    });
+  };
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Order extends Component {
+  render() {
+    return <div className="order" />;
+  }
+}
+
+export default Order;
